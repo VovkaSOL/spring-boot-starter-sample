c4e603d141b78476af93e4dafef2c2164858bf1a
Vladimir Solonchenko <vovkasol@mail.ru>
2021-02-07 19:57:01 +0300

    custom starter and aop

--- sample-spring-boot-starter-example/src/main/java/net/aimeizi2/SampleApplication.java
+++ sample-spring-boot-starter-example/src/main/java/net/aimeizi2/SampleApplication.java
@@ -1,13 +1,18 @@
-package net.aimeizi;
+package net.aimeizi2;
 
+import net.aimeizi.SampleService;
+import org.aspectj.lang.annotation.Around;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class SampleApplication {
 
     public static void main(String[] args) {
@@ -20,5 +25,11 @@ public class SampleApplication {
     @GetMapping("/input")
     public String output(String word) {
         return sampleService.wrap(word);
+    }
+    @Scheduled(fixedDelay = 1000)
+    @LogExecutionTime
+    public void scheduleFixedDelayTask() {
+        System.out.println(
+                "Fixed delay task - " + System.currentTimeMillis() / 1000);
     }
 }
